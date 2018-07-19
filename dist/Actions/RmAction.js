214788504ada021ca5e4f9b40f78f5476e70c7e5
Olaf Erlandsen <olaftriskel@gmail.com>
2018-07-19 14:54:44 -0400

    fix script

--- dist/Actions/RmAction.js
+++ dist/Actions/RmAction.js
@@ -1,6 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const AddAction_1 = require("./AddAction");
+const Helpers_1 = require("../Helpers");
+let path = require("path");
+let fs = require("fs");
+let c = require("ansi-colors");
 class RmAction {
     constructor(argv) {
         AddAction_1.AddAction.argv = argv;
@@ -8,14 +12,39 @@ class RmAction {
             case "controller":
                 this.controller.apply(this, argv.slice(1));
                 break;
-            case "http":
-                this.controller.apply(this, argv.slice(1));
-                break;
         }
     }
-    controller() {
-    }
-    http() {
+    controller(controller, rootRoute) {
+        controller = Helpers_1.Helpers.sanitizeUri(controller.replace('\\', '/'));
+        rootRoute = Helpers_1.Helpers.sanitizeUri(rootRoute);
+        let segments = controller.split('/').map(v => Helpers_1.Helpers.camelize(v));
+        if (segments.length === 0)
+            throw new Error("controller is empty");
+        let name = Helpers_1.Helpers.camelize(segments[segments.length - 1]);
+        let dirPath = [AddAction_1.AddAction.directory, "src", "controllers"]
+            .concat(segments.slice(0, segments.length - 1))
+            .join(path.sep);
+        let ctrlPath = dirPath + path.sep + name + "Controller.ts";
+        let route = (rootRoute || "" || name)
+            .trim()
+            .replace(/(^\/+|\/+$)/g, '')
+            .replace(/\/+/g, '/');
+        if (fs.existsSync(ctrlPath)) {
+            fs.unlink(ctrlPath, (err) => {
+                if (err) {
+                    console.log(c.bold.gray(c.bold.red(c.symbols.cross), `The controller ${name} could not be remove: ${err.toString()}`));
+                    process.exit(1);
+                }
+                else {
+                    console.log(c.bold.gray(c.bold.green(c.symbols.check), `The controller ${name} was remove correctly`));
+                    process.exit(0);
+                }
+            });
+        }
+        else {
+            console.log(c.bold.gray(c.bold.yellow(c.symbols.warning), `The controller ${name} does not exists`));
+            process.exit(0);
+        }
     }
 }
 RmAction.argv = [];
