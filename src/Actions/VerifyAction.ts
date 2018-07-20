b07d34b88fa24104523fcea061f69f79eeb760be
Olaf Erlandsen <olaftriskel@gmail.com>
2018-07-20 13:11:02 -0400

    remove unnecesary elements of verify

--- src/Actions/VerifyAction.ts
+++ src/Actions/VerifyAction.ts
@@ -8,27 +8,20 @@ export class VerifyAction {
     public static args : string [] = [];
     public static directory : string;
     public static stats : any = {
-        positive : 0,
-        negative : 0,
-        exists : 0,
-        dontExists : 0
+        negative : 0
     };
     public static files : Array<{
         name : string,
-        type : "file" | "dir" | "symbolic",
-        cheksum?: string
+        type : "file" | "dir" | "symbolic"
     }> = [
-
         {name : "package.json", type : "file"},
         {name : "gulpfile.js", type : "file"},
         {name : "tsconfig.json", type : "file"},
-
         {name : "src", type : "dir"},
         {name : "src/manifiest.json", type : "file"},
         {name : "src/typings.d.ts", type : "file"},
         {name : "src/server.ts", type : "file"},
         {name : "src/app.ts", type : "file"},
-
         {name : "src/core", type : "dir"},
         {name : "src/core/Annotations.ts", type : "file"},
         {name : "src/core/Controller.ts", type : "file"},
@@ -36,8 +29,6 @@ export class VerifyAction {
         {name : "src/core/Manifiest.ts", type : "file"},
         {name : "src/core/Middleware.ts", type : "file"},
         {name : "src/core/MiddlewareInject.ts", type : "file"},
-
-
         {name : "src/annotations", type : "dir"},
         {name : "src/annotations/Api.ts", type : "file"},
         {name : "src/annotations/Http.ts", type : "file"},
@@ -48,7 +39,7 @@ export class VerifyAction {
         VerifyAction.directory = process.cwd();
         VerifyAction.args = args;
         let message : string;
-        if (!this.isValid()) message = "This project is not valid:";
+        if (!VerifyAction.isValid()) message = "This project is not valid:";
         else message = "This project is valid:";
         console.log(
             c.bold.gray(
@@ -63,53 +54,32 @@ export class VerifyAction {
         process.exit(VerifyAction.stats.negative === 0 ? 0 : 1);
     }
 
-    public isValid ():boolean {
-
+    static isValid ():boolean {
         for (let file of VerifyAction.files) {
             let filePath: string = VerifyAction.directory + path.sep + file.name;
-            if (!this.exists(filePath)) {
-                VerifyAction.stats.negative++;
-                VerifyAction.stats.dontExists++;
-            }
+            if (!VerifyAction.exists(filePath)) VerifyAction.stats.negative++;
             else {
-                VerifyAction.stats.exists++;
-                if (file.type === "dir" && !this.isDir(filePath)) {
-                    VerifyAction.stats.negative++;
-                }
-                if (file.type === "file" && !this.isFile(filePath)) {
-                    VerifyAction.stats.negative++;
-                }
-                if (file.type === "symbolic" && !this.isSymbolicLink(filePath)) {
-                    VerifyAction.stats.negative++;
-                }
-                if (typeof file.cheksum === "string") {
-                    if (this.checksum(filePath) !== file.cheksum) {
-                        VerifyAction.stats.negative++;
-                    }
-                }
+                if (
+                    (file.type === "dir" && !VerifyAction.isDir(filePath))
+                    || (file.type === "file" && !VerifyAction.isFile(filePath))
+                    || (file.type === "symbolic" && !VerifyAction.isSymbolicLink(filePath))
+                ) VerifyAction.stats.negative++;
             }
         }
         return VerifyAction.stats.negative === 0;
     }
 
 
-    public checksum (filePath : string):string {
-        return crypto
-            .createHash("sha1")
-            .update(fs.readFileSync(filePath, "utf8"), 'utf8')
-            .digest('hex')
-    }
-
-    public isDir (path :string):boolean {
+    static isDir (path :string):boolean {
         return fs.lstatSync(path).isDirectory()
     }
-    public isFile (path : string):boolean {
+    static isFile (path : string):boolean {
         return fs.lstatSync(path).isFile()
     }
-    public isSymbolicLink (path : string):boolean {
+    static isSymbolicLink (path : string):boolean {
         return fs.lstatSync(path).isSymbolicLink()
     }
-    public exists (path: string):boolean {
+    static exists (path: string):boolean {
         return fs.existsSync(path);
     }
 
